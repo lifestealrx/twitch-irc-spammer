d37bc1ef1d3358106d4bcf87a460099f88651999
lifestealrx <xjedi@myself.com>
2015-12-07 19:36:50 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import socket, time
+
+channel = '#twitchuser'
+
+sockets = []
+
+class irc:
+    @staticmethod
+    def main(channel, user, pw, sock):
+        sock.send('PASS %s\r\n' %pw)
+        sock.send('NICK %s\r\n' %user)
+        sock.send('JOIN %s\r\n' %channel)
+		
+def main():
+    with open('accounts', 'r') as accts:
+        for act in accts:
+            acct = act.split(':', 1)
+
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect(('irc.twitch.tv', 6667))
+
+            sockets.append(sock)
+            
+            irc.main(channel,
+                    acct[0],
+                    acct[1],
+                    sock)
+
+main()
+
+exit()
